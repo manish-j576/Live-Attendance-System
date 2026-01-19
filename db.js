d93f661ed16c0381da2eff484dbdf48fc2f0b866
Manish Joshi <manishjoshi705572@gmail.com>
2026-01-19 12:50:11 +0530

    Added Zod for signup request

---
+++ db.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv"
+import mongoose, { Schema }  from "mongoose";
+dotenv.config()
+
+export const connectDB =async () => {
+    await mongoose.connect(process.env.DATABASE_URL)
+    console.log("Database connected successfully")
+}
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: String,
+  role: { type: String, required: true, enum: ["teacher", "student"] },
+});
+
+const ClassSchema = new Schema({
+  className: String,
+  teacherId: mongoose.ObjectId, // reference to User
+  studentIds: [mongoose.ObjectId] // array of User references
+})
+
+const AttendanceSchema = new Schema({
+  classId: mongoose.ObjectId,
+  studentId: mongoose.ObjectId,
+  status:  { type: String, required: true, enum: ["present", "absent"]}
+});
+
+
+export const User = mongoose.model("User" , UserSchema)
+
+export const Class = mongoose.model("Class" , ClassSchema)
+
+export const Attendance = mongoose.model("Attendance" , AttendanceSchema)
--- server.js
+++ server.js
@@ -1,38 +1,69 @@
 import express from "express"
 import dotenv from "dotenv"
 import bodyParser from "body-parser"
+import { connectDB, User } from "./db.js"
+import mongoose from "mongoose"
+import z, { email } from "zod"
+
 dotenv.config()
 const app = express()
 app.use(bodyParser.json())// body parser 
 
+connectDB()
+const signupRequest = z.object({
+    name : z.string(),
+    email : z.email(),
+    password : z.string(),
+    role : z.string("teacher" | "student")
+})
+
+
 // signup endpoint 
 app.post("/signup",async (req,res)=>{
 
-    // console.log(req.body)
-    const {name , email , password , role } = req.body
-    // console.log(
-    //     name,
-    //     email,
-    //     password,
-    //     role
-    // )
-    // parse the data with zod
-
-    // find the user in the db 
-
-    // if not found create entry in the db
 
 
-    // return the response after creating the user
+    try{
 
-
-    return res.json({
-        success : true,
-        data : {
-            name ,
-            email 
+        const data = signupRequest.parse(req.body)
+        // console.log(req.body)
+        const {name , email , password , role } = req.body
+        // parse the data with zod
+        
+        // find the user in the db 
+        const user = await User.findOne({
+            email
+        })
+        if(user) {
+            console.log("user already exist")
+            res.json({
+                success : false,
+                error : "User already exist"
+            })
         }
-    })
+        // if not found create entry in the db
+        const response =await User.create({
+            name,
+            email,
+            password,
+            role
+        })
+
+        console.log("User created successfully")
+        // return the response after creating the user
+        return res.json({
+            success : true,
+            data : {
+                name ,
+                email 
+            }
+        })
+    }catch(error){
+        return res.json({
+            success : false,
+            error : error
+        })
+    }
 })
 
 app.post("/login", (req, res) =>{
