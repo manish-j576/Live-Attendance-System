e307e75fed964db61a574a7187c2c8ae1ee3b604
Manish Joshi <manishjoshi705572@gmail.com>
2026-01-21 09:21:48 +0530

    Added class API route & add-student requrire fix

--- authmiddleware.js
+++ authmiddleware.js
@@ -5,9 +5,34 @@ export async function auth(req, res , next){
 
         const token = req.headers.authorization
         const user = jwt.verify(token , process.env.JWT_SECRET)
+        req.userID = user.userId
         next()
     }catch(error){
         return res.status(401).json({
+          success: false,
+          error: "Unauthorized, token missing or invalid",
+        });
+    }
+}
+
+export async function teacherAuth(req, res , next){
+    try{
+        console.log("teahers auth here")
+        const token = req.headers.authorization
+        const user = jwt.verify(token , process.env.JWT_SECRET)
+        console.log("Middleware ")
+        if( user.role != "teacher"){
+            return res.status(403).json({
+              success: false,
+              error: "Forbidden, teacher access required",
+            });
+        } 
+
+        req.userID = user.userId
+        next()
+
+    }catch(error){
+        res.status(401).json({
           success: false,
           error: "Unauthorized, token missing or invalid",
         });
--- server.js
+++ server.js
@@ -1,173 +1,257 @@
 import express from "express"
 import dotenv from "dotenv"
 import bodyParser from "body-parser"
-import { connectDB, User } from "./db.js"
-import mongoose from "mongoose"
-import z, { email } from "zod"
+import { Class, connectDB, User } from "./db.js"
+import z, { email, success } from "zod"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-import { auth } from "./authmiddleware.js"
+import { auth, teacherAuth } from "./authmiddleware.js"
 dotenv.config()
 const app = express()
 app.use(bodyParser.json())// body parser 
-async function main () {
-    console.log("app running")
-    
-    await connectDB()
-    const signupRequest = z.object({
-        name : z.string(),
-        email : z.email(),
-        password : z.string().min(6),
-        role : z.string("teacher" | "student")
-    })
 
-const loginRequestSchema = z.object({
+
+
+async function main() {
+  console.log("app running");
+
+  await connectDB();
+
+  const signupRequest = z.object({
+    name: z.string(),
     email: z.email(),
-    password : z.string().min(6)
+    password: z.string().min(6),
+    role: z.string("teacher" | "student"),
+  }).strict();
+
+  const loginRequestSchema = z
+    .object({
+      email: z.email(),
+      password: z.string().min(6),
     })
-// signup endpoint 
-app.post("/auth/signup",async (req,res)=>{
+    .strict();
 
-    console.log("signup endpoint hit")
-    try{
-        // parse the data with zod
-        const data = signupRequest.parse(req.body)
-        
-        
-        console.log("control reacheds herr")
-        const {name , email , password , role } = data
-        console.log("control reacheds herr 2")
-        
-        // find the user in the db 
-        const user = await User.findOne({
-            email
-        })
-        console.log("control reacheds herr 3")
-        if(user) {
-            console.log("user already exist")
-            return res.status(400).json({
-                success: false,
-                error: "Email already exists",
-            });
-        }
-        console.log("control reacheds herr 4")
-        
-        //hash the password in using bcrypt
-        const hashedPassword = await bcrypt.hash(password , Number(process.env.SALT_ROUNDS))
-        console.log("control reacheds herr 5")
-        
-        // if not found create entry in the db
-        const response = await User.create({
-            name,
-            email,
-            password : hashedPassword,
-            role
-        })
-        console.log("control reacheds herr 6")
-        
-        
-        
-        // return the response after creating the user
-        return res.status(201).json({
-            success : true,
-            data : {
-                id : response._id,
-                name  : response.name,
-                email : response.email,
-                role : response.role
-            }
-        }) 
-        
-        
-    }catch(error){ 
-        console.log("control reacheds herr 7")
-        console.log(error)
-        return res.status(400).json({
-            success: false,
-            error: "Invalid request schema",
+  const classRequestSchema = z
+    .object({
+      className: z.string(),
+    })
+    .strict();
+
+    const addStudentRequestSchema = z.object({
+        studentId : z.string()
+    }).strict()
+
+
+  // signup endpoint
+  app.post("/auth/signup",async (req,res)=>{
+      try{
+          // parse the data with zod
+          const data = signupRequest.parse(req.body)
+
+          const {name , email , password , role } = data
+
+          // find the user in the db
+          const user = await User.findOne({
+              email
+          })
+
+          if(user) {
+              return res.status(400).json({
+                  success: false,
+                  error: "Email already exists",
+              });
+          }
+
+          //hash the password in using bcrypt
+          const hashedPassword = await bcrypt.hash(password , Number(process.env.SALT_ROUNDS))
+
+          // if not found create entry in the db
+          const response = await User.create({
+              name,
+              email,
+              password : hashedPassword,
+              role
+          })
+
+          // return the response after creating the user
+          return res.status(201).json({
+              success : true,
+              data : {
+                  id : response._id,
+                  name  : response.name,
+                  email : response.email,
+                  role : response.role
+              }
+          })
+
+      }catch(error){
+          return res.status(400).json({
+              success: false,
+              error: "Invalid request schema",
+          });
+      }
+  })
+
+  app.post("/auth/login", async (req, res) => {
+    try {
+      // check the zod schema
+      const data = loginRequestSchema.parse(req.body);
+
+      const { email, password } = data;
+
+      // make the db call and find the user
+      const user = await User.findOne({
+        email,
+      });
+
+      // if not found return with the user not found error
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          error: "User not found",
         });
-    }
-})
+      }
 
-app.post("/auth/login",async (req, res) =>{
-    
-    try{
-        
-        // check the zod schema
-        const data = loginRequestSchema.parse(req.body)
-        
-        const {email , password } = data
-        
-        // make the db call and find the user
-        const user = await User.findOne({
-            email
-        })
-        
-        // if not found return with the user not found error
-        if(!user){
-            return res.status(404).json({
-                success: false, 
-                error : "User not found"
-                
-            })
-        }
-        
-        
-        // if ifound check for the password
-        const isMatch = await bcrypt.compare(password , user.password )
-        // return the response if the password is incorrect
-        if(!isMatch){
-            return res.status(400).json({
-                success: false,
-                error: "Invalid email or password",
-            });
-        }
-        
-        // if correct signs a jwt and return to the user 
-        const token = jwt.sign({
-            userId : user._id,
-            role : user.role
-        },process.env.JWT_SECRET)
-        
-        return res.json({
-            success : true,
-            data  : {
-                token : token
-            }
-        })
-    }catch(error){
-
-
-        console.log("error occured")
-        console.log(error)
-
+      // if ifound check for the password
+      const isMatch = await bcrypt.compare(password, user.password);
+      // return the response if the password is incorrect
+      if (!isMatch) {
         return res.status(400).json({
           success: false,
-          error: "Invalid request schema",
+          error: "Invalid email or password",
         });
+      }
+
+      // if correct signs a jwt and return to the user
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          role: user.role,
+        },
+        process.env.JWT_SECRET,
+      );
+
+      return res.json({
+        success: true,
+        data: {
+          token: token,
+        },
+      });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid request schema",
+      });
     }
-})
+  });
 
-app.get("/auth/me" ,auth ,async  (req , res) =>{
-    console.log("get end point ")
+  app.get("/auth/me", auth, async (req, res) => {
     const user = await User.findOne({
-        "_id" : req.userId
-    })
-
+      _id: req.userID,
+    });
 
     return res.json({
-        success : true,
-        data : {
-            _id : user._id,
-            name : user.name,
-            email : user.email,
-            role : user.role
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  });
+
+  app.post("/class", teacherAuth, async (req, res) => {
+    try {
+      //zod validation
+      const data = classRequestSchema.parse(req.body);
+
+      // classs send request
+      const response = await Class.create({
+        className: data.className,
+        teacherId: req.userID,
+        studentIds: [],
+      });
+
+      console.log(response);
+      return res.status(201).json({
+        success: true,
+        data: {
+          _id: response._id,
+          className: response.className,
+          teacherId: response.teacherId.toHexString(),
+          studentIds: response.studentIds,
+        },
+      });
+    } catch (e) {
+      res.status(400).json({
+        success: false,
+        error: "Invalid request schema",
+      });
+    }
+  });
+
+  app.post( "/class/:id/add-student", teacherAuth, async (req, res) => {
+    console.log("request in .here");
+    try {
+      //zod validation
+      console.log("1")
+      const data = addStudentRequestSchema.parse(req.body);
+      console.log("2")
+      const classId = req.params.id;
+      console.log("3")
+      
+      const foundClass = await Class.findOne({
+          _id: classId,
+        });
+        console.log("4")
+        
+        if (!foundClass) {
+            return res.status(404).json({
+                success: false,
+                error: "Class not found",
+            });
         }
-    })})
+        console.log("5")
+        
+        // if we found the class
+        let newStudentId = []
+        newStudentId = [...foundClass.studentIds , data.studentId]
+        console.log(newStudentId)
+        console.log("6")
+        
+        const updatedClass = Class.updateOne(
+            { _id: classId },
+            { studentIds: newStudentId },
+        );
+        console.log("7")
+        
+        // fetch user again 
+        const fetchClass = await Class.findOne({
+            _id : classId
+        })
+        console.log(fetchClass)
+        console.log("$$$$$$$$$$$$$$$")
+        console.log(updatedClass);
+        console.log("8")
+        return res.status(200).json({
+            success: true,
+            data: {
+                _id: foundClass._id,
+                className: foundClass.className,
+                teacherId: foundClass.teacherId,
+                studentIds: "updatedClass._update.studentIds",
+            },
+        });
+        console.log("9")
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        error: "Invalid request schema",
+      });
+    }
+  });
 
-app.listen(process.env.PORT)
-
-
+app.listen(process.env.PORT);
 }
-
 main()
